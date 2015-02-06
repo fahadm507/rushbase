135cb539412d7c1e29ee6952e23a50de5f82afa1
Fahad Mukunzi <fahadm@painless-security.com>
2015-02-05 19:13:10 -0500

    Initial commit
    * create rails app
    
    * add devise gem and initial set up
    
    * add bootstrap-sass gem and set up
    
    * add users controller, specs and views scaffold

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,5 @@
+class WelcomeController < ApplicationController
+  def index
+  	#redirects to the welcome index page
+  end
+end
---
+++ spec/controllers/welcome_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+define WelcomeController do 
+
+end
