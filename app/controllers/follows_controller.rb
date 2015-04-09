6d4280ec654a9e7ca28408b166c29d276d395c6e
Fahad Mukunzi <fahadm@painless-security.com>
2015-04-09 18:10:26 -0400

    impliment followers functionality(WIP)

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,21 +1,25 @@
-class FollowController < ApplicationController
+class FollowsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :destroy]
-
   def create
     @user = User.find(params[:id])
     respond_to do |format|
       if current_user.follow(@user)
-        format.html { render 'users/following'}
+        format.js {} 
+      else
         format.js {}
-        format.json { render json: @user }
       end
     end
   end
 
   def destroy
-    @unfollow = Follow.find(params[:id])
-    if current_user.stop_following(@ufollow)
-      render "user/following"
+    binding.pry
+    @unfollow = Follow.find_by(followable_id: params[:id])
+    respond_to do |format|
+      if current_user.stop_following(@ufollow)
+        format.js {}
+      else
+        format.js {}
+      end
     end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:create, :update, :destroy]
   respond_to :json
-
   def index
     @users = User.all
     respond_to do |format|
@@ -13,10 +12,12 @@ class UsersController < ApplicationController
   def show
     @user_post = UserPost.new
     @user_posts = UserPost.all
-    if user_signed_in?
-      @user = current_user
-    else
+    
+    if (params[:id])
       @user = User.find(params[:id])
+    end
+    if @user == current_user
+      redirect_to profile_path
     end
   end
 
