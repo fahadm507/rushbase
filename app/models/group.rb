8c43c07eb119bbecf2da7a6e0e10ca1c91638c31
Fahad Mukunzi <fahadm507@gmail.com>
2015-07-31 01:36:30 -0400

    Add ability to create groups from third party courses

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,42 @@ class Group < ActiveRecord::Base
   has_many   :group_members
   has_many   :users, through: :group_members
   belongs_to :user
+
+  include HTTParty
+
+  base_uri 'www.udacity.com'
+  belongs_to :user
+  has_many :meetup_members
+  has_many :meetup_posts
+  has_many :users, through: :meetup_members
+
+  def member?(user)
+    self.users.include?(user)
+  end
+
+  def self.create_groups
+    response = HTTParty.get("https://www.udacity.com/public-api/v0/courses")
+    courses = response["courses"]
+
+    courses.each do |course|
+      self.find_or_create_by(
+        name: course['title'],
+        provider: "Udacity",
+        banner_image: course['banner_image'],
+        course_link: course['course_link']
+        )
+    end
+  end
+
+  def self.create_coursera_groups
+    coursera_res = HTTParty.get("https://api.coursera.org/api/catalog.v1/courses")
+    courses = coursera_res["elements"]
+
+    courses.each do |course|
+      self.find_or_create_by(
+        name: course['name'],
+        provider: "Coursera"
+      )
+    end
+  end
 end
